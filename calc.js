5ec4bcf156dae06daff8e35a12f27fff01ee21ef
Adrie <adriebuendia@icloud.com>
2022-04-09 17:47:26 -0400

    Removed Unwanted code

--- calc.js
+++ calc.js
@@ -1,6 +1,4 @@
 const readline = require('readline');
-const { CLIENT_RENEG_LIMIT } = require('tls');
-
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
